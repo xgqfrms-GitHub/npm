8d36ba24b9b56c8c8b777b403eb92d0b5afe2c78
Kat Marchán <kzm@sykosomatic.org>
2016-08-30 20:19:42 -0700

    test: updated tests for updateIndex

--- test/tap/update-index.js
+++ test/tap/update-index.js
@@ -1,200 +1,209 @@
+'use strict'
+
 var common = require('../common-tap.js')
-var test = require('tap').test
 var npm = require('../../')
+var test = require('tap').test
 var mkdirp = require('mkdirp')
 var rimraf = require('rimraf')
 var path = require('path')
+var fs = require('fs')
+var cacheFile = require('npm-cache-filename')
 var mr = require('npm-registry-mock')
+var ms = require('mississippi')
+var Tacks = require('tacks')
+var File = Tacks.File
 
 var updateIndex = require('../../lib/cache/update-index.js')
 
-var PKG_DIR = path.resolve(__dirname, 'get-basic')
+var PKG_DIR = path.resolve(__dirname, 'update-index')
 var CACHE_DIR = path.resolve(PKG_DIR, 'cache')
+var cacheBase
+var cachePath
 
 var server
 
-var mocks = {
-  basic: function (mock) {
-    mock.get('/-/all').reply(200, allMock)
-  },
-  auth: function (mock) {
-    var littleBobbyTablesAuth = new Buffer('bobby:tables').toString('base64')
-    var auth = 'Basic ' + littleBobbyTablesAuth
-    mock.get('/-/all', { authorization: auth }).reply(200, allMock)
-    mock.get('/-/all').reply(401, {
-      error: 'unauthorized',
-      reason: 'You are not authorized to access this db.'
-    })
-  }
+function setup () {
+  mkdirp.sync(cacheBase)
 }
 
-function noFilter () {
-  return true
+function cleanup () {
+  rimraf.sync(PKG_DIR)
 }
 
-var allMock = {
-  '_updated': 1411727900 + 25,
-  'generator-frontcow': {
-    'name': 'generator-frontcow',
-    'description': 'f36b6a6123da50959741e2ce4d634f96ec668c56 This is a fake description to ensure we do not accidentally search the real npm registry or use some kind of cache',
-    'dist-tags': {
-      'latest': '0.1.19'
-    },
-    'maintainers': [
-      {
-        'name': 'bcabanes',
-        'email': 'contact@benjamincabanes.com'
-      }
-    ],
-    'homepage': 'https://github.com/bcabanes/generator-frontcow',
-    'keywords': [
-      'sass',
-      'frontend',
-      'yeoman-generator',
-      'atomic',
-      'design',
-      'sass',
-      'foundation',
-      'foundation5',
-      'atomic design',
-      'bourbon',
-      'polyfill',
-      'font awesome'
-    ],
-    'repository': {
-      'type': 'git',
-      'url': 'https://github.com/bcabanes/generator-frontcow'
-    },
-    'author': {
-      'name': 'ben',
-      'email': 'contact@benjamincabanes.com',
-      'url': 'https://github.com/bcabanes'
-    },
-    'bugs': {
-      'url': 'https://github.com/bcabanes/generator-frontcow/issues'
-    },
-    'license': 'MIT',
-    'readmeFilename': 'README.md',
-    'time': {
-      'modified': '2014-10-03T02:26:18.406Z'
-    },
-    'versions': {
-      '0.1.19': 'latest'
-    }
-  },
-  'marko': {
-    'name': 'marko',
-    'description': 'Marko is an extensible, streaming, asynchronous, high performance, HTML-based templating language that can be used in Node.js or in the browser.',
-    'dist-tags': {
-      'latest': '1.2.16'
-    },
-    'maintainers': [
-      {
-        'name': 'pnidem',
-        'email': 'pnidem@gmail.com'
-      },
-      {
-        'name': 'philidem',
-        'email': 'phillip.idem@gmail.com'
-      }
-    ],
-    'homepage': 'https://github.com/raptorjs/marko',
-    'keywords': [
-      'templating',
-      'template',
-      'async',
-      'streaming'
-    ],
-    'repository': {
-      'type': 'git',
-      'url': 'https://github.com/raptorjs/marko.git'
-    },
-    'author': {
-      'name': 'Patrick Steele-Idem',
-      'email': 'pnidem@gmail.com'
-    },
-    'bugs': {
-      'url': 'https://github.com/raptorjs/marko/issues'
-    },
-    'license': 'Apache License v2.0',
-    'readmeFilename': 'README.md',
-    'users': {
-      'pnidem': true
-    },
-    'time': {
-      'modified': '2014-10-03T02:27:31.775Z'
-    },
-    'versions': {
-      '1.2.16': 'latest'
-    }
-  }
-}
-
-function setup (t, mock, extra) {
-  mkdirp.sync(CACHE_DIR)
-  mr({ port: common.port, plugin: mock }, function (er, s) {
+test('setup', function (t) {
+  mr({port: common.port, throwOnUnmatched: true}, function (err, s) {
+    t.ifError(err, 'registry mocked successfully')
     npm.load({ cache: CACHE_DIR, registry: common.registry }, function (err) {
-      if (extra) {
-        Object.keys(extra).forEach(function (k) {
-          npm.config.set(k, extra[k], 'user')
-        })
-      }
-      t.ifError(err, 'no error')
+      t.ifError(err, 'npm loaded successfully')
+      npm.config.set('cache', CACHE_DIR)
+      cacheBase = cacheFile(npm.config.get('cache'))(common.registry + '/-/all')
+      cachePath = path.join(cacheBase, '.cache.json')
       server = s
+      t.pass('all set up')
+      t.done()
+    })
+  })
+})
+
+test('updateIndex full request', function (t) {
+  setup()
+  var updated = 1234
+  server.get('/-/all').once().reply(200, {
+    '_updated': updated,
+    'bar': { name: 'bar', version: '1.0.0' },
+    'foo': { name: 'foo', version: '1.0.0' }
+  }, {
+    date: Date.now() // should never be used.
+  })
+  updateIndex(600, function (err, stream) {
+    if (err) throw err
+    t.ok(stream, 'returned a stream')
+    var results = []
+    stream.on('data', function (pkg) {
+      results.push(pkg)
+    })
+    ms.finished(stream, function (err) {
+      if (err) throw err
+      t.deepEquals(results, [{
+        name: 'bar',
+        version: '1.0.0'
+      }, {
+        name: 'foo',
+        version: '1.0.0'
+      }])
+      var fileData = JSON.parse(fs.readFileSync(cachePath))
+      t.ok(fileData, 'cache contents written to the right file')
+      t.deepEquals(fileData, {
+        '_updated': updated,
+        bar: {
+          name: 'bar',
+          version: '1.0.0'
+        },
+        foo: {
+          name: 'foo',
+          version: '1.0.0'
+        }
+      }, 'cache contents based on what was written')
+      server.done()
+      cleanup()
       t.end()
     })
   })
-}
+})
 
-function cleanup (t) {
-  server.close(function () {
-    rimraf.sync(PKG_DIR)
+test('updateIndex cache only', function (t) {
+  setup()
+  var now = Date.now()
+  var cacheContents = {
+    '_updated': now,
+    bar: { name: 'bar', version: '1.0.0' },
+    cool: { name: 'cool', version: '1.0.0' },
+    foo: { name: 'foo', version: '2.0.0' },
+    other: { name: 'other', version: '1.0.0' }
+  }
+  var fixture = new Tacks(File(cacheContents))
+  fixture.create(cachePath)
+  // TODO - figure out how to get the time stuff in here
+  updateIndex(10000000, function (err, stream) {
+    if (err) throw err
+    t.ok(stream, 'returned a stream')
+    var results = []
+    stream.on('data', function (pkg) {
+      results.push(pkg)
+    })
+    ms.finished(stream, function (err) {
+      t.ifError(err, 'stream finished without error')
+      t.deepEquals(
+        results.map(function (pkg) { return pkg.name }),
+        ['bar', 'cool', 'foo', 'other'],
+        'packages deduped and sorted, without _updated'
+      )
+      var fileData = JSON.parse(fs.readFileSync(cachePath))
+      t.ok(fileData, 'cache contents written to the right file')
+      t.deepEquals(fileData, cacheContents, 'cacheContents written directly')
+      server.done()
+      cleanup()
+      t.end()
+    })
+  })
+})
 
+test('createEntryStream merged stream', function (t) {
+  setup()
+  var now = Date.now()
+  var cacheTime = now - 100000
+  var reqTime = (new Date(now)).toISOString()
+  server.get('/-/all/since?stale=update_after&startkey=' + cacheTime).once().reply(200, {
+    'bar': { name: 'bar', version: '2.0.0' },
+    'car': { name: 'car', version: '1.0.0' },
+    'foo': { name: 'foo', version: '1.0.0' }
+  }, {
+    date: reqTime
+  })
+  var fixture = new Tacks(File({
+    '_updated': cacheTime,
+    bar: { name: 'bar', version: '1.0.0' },
+    cool: { name: 'cool', version: '1.0.0' },
+    foo: { name: 'foo', version: '2.0.0' },
+    other: { name: 'other', version: '1.0.0' }
+  }))
+  fixture.create(cachePath)
+  updateIndex(600, function (err, stream) {
+    if (err) throw err
+    t.ok(stream, 'returned a stream')
+    var results = []
+    stream.on('data', function (pkg) {
+      results.push(pkg)
+    })
+    ms.finished(stream, function (err) {
+      t.ifError(err, 'stream finished without error')
+      t.deepEquals(
+        results.map(function (pkg) { return pkg.name }),
+        ['bar', 'car', 'cool', 'foo', 'other'],
+        'packages deduped and sorted'
+      )
+      t.deepEquals(results[0], {
+        name: 'bar',
+        version: '2.0.0'
+      }, 'update stream version wins on dedupe')
+      t.deepEquals(results[3], {
+        name: 'foo',
+        version: '1.0.0'
+      }, 'update stream version wins on dedupe even when the newer one has a lower semver.')
+      var cacheContents = {
+        '_updated': Date.parse(reqTime),
+        bar: { name: 'bar', version: '2.0.0' },
+        car: { name: 'car', version: '1.0.0' },
+        cool: { name: 'cool', version: '1.0.0' },
+        foo: { name: 'foo', version: '1.0.0' },
+        other: { name: 'other', version: '1.0.0' }
+      }
+      var fileData = JSON.parse(fs.readFileSync(cachePath))
+      t.ok(fileData, 'cache contents written to the right file')
+      t.deepEquals(fileData, cacheContents, 'cache updated correctly')
+      server.done()
+      cleanup()
+      t.end()
+    })
+  })
+})
+
+test('updateIndex no sources', function (t) {
+  setup()
+  server.get('/-/all').once().reply(404, {})
+  updateIndex(600, function (err, stream) {
+    t.ok(err, 'no sources, got an error')
+    t.notOk(stream, 'no stream returned')
+    t.match(err.message, /No search sources available/, 'useful error message')
+    server.done()
+    cleanup()
     t.end()
   })
-}
-
-test('setup basic', function (t) {
-  setup(t, mocks.basic)
 })
 
-test('request basic', function (t) {
-  updateIndex(0, null, null, noFilter, function (er) {
-    t.ifError(er, 'no error')
-    t.end()
-  })
+test('cleanup', function (t) {
+  cleanup()
+  server.close()
+  t.pass('all done')
+  t.done()
 })
-
-test('cleanup basic', cleanup)
-
-test('setup auth', function (t) {
-  setup(t, mocks.auth)
-})
-
-test('request auth failure', function (t) {
-  updateIndex(0, null, null, noFilter, function (er) {
-    t.ok(er, 'got an error')
-    t.equals(er.code, 'E401', 'gotta get that auth')
-    t.ok(/^unauthorized/.test(er.message), 'unauthorized message')
-    t.end()
-  })
-})
-
-test('cleanup auth failure', cleanup)
-
-test('setup auth', function (t) {
-  // mimic as if alwaysAuth had been set
-  setup(t, mocks.auth, {
-    _auth: new Buffer('bobby:tables').toString('base64'),
-    'always-auth': true
-  })
-})
-
-test('request auth success', function (t) {
-  updateIndex(0, null, null, noFilter, function (er) {
-    t.ifError(er, 'no error')
-    t.end()
-  })
-})
-
-test('cleanup auth', cleanup)
